1d31124ba73c40b22144aa50c37233f1dd03eefd
Manuel Brea <m.brea.carreras@gmail.com>
2022-10-18 16:39:11 +0100

    Added test change

--- lua/telescope/_extensions/unicode-input/find.lua
+++ lua/telescope/_extensions/unicode-input/find.lua
@@ -2,13 +2,24 @@ local M = {}
 
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
-local sorters = require("telescope.sorters")
+--local sorters = require("telescope.sorters")
 local actions = require("telescope.actions")
+local conf = require("telescope.config").values
 local action_state = require("telescope.actions.state")
 local entry_display = require("telescope.pickers.entry_display")
 local results = require("telescope-unicode-input").opts.unicode
 
+local sample_unicode = {
+  { value = "ℕ", key = "bN", name = "Double-Struck Capital N" },
+  { value = "→", key = "->", name = "Rightwards Arrow" },
+  { value = "∸", key = ".-", name = "Dot Minus" },
+  { value = "≡", key = "==", name = "Identical To" },
+}
+
+--local results = sample_unicode
+
 M.search = function(telescope_opts)
+  telescope_opts = telescope_opts or {}
   local displayer = entry_display.create({
     separator = " ",
     items = {
@@ -25,34 +36,37 @@ M.search = function(telescope_opts)
     })
   end
   pickers
-    .new(telescope_opts, {
-      prompt_title = "telescope-agda-symbols",
-      finder = finders.new_table({
-        results = results,
-        entry_maker = function(result)
-          return {
-            value = result.value,
-            name = result.name,
-            key = result.key,
-            display = make_display,
-            ordinal = result.key .. result.name,
-          }
+      .new(telescope_opts, {
+        prompt_title = "telescope-agda-symbols",
+        finder = finders.new_table({
+          results = results,
+          entry_maker = function(result)
+            return {
+              value = result.value,
+              name = result.name,
+              key = result.key,
+              display = make_display,
+              ordinal = result.key,
+            }
+          end,
+        }),
+        sorter = conf.generic_sorter(telescope_opts),
+        attach_mappings = function(prompt_bufnr, _)
+          actions.select_default:replace(actions.insert_symbol_i)
+          --actions.select_default:replace(function()
+          --local selection = action_state.get_selected_entry()
+          --actions.close(prompt_bufnr)
+          --local output = selection.value
+          --local pos = vim.api.nvim_win_get_cursor(0)
+          --vim.api.nvim_buf_set_text(0, pos[1] - 1, pos[2], pos[1] - 1, pos[2], { output })
+          --vim.api.nvim_win_set_cursor(0, { pos[1], pos[2] + 2 })
+          --end)
+          return true
         end,
-      }),
-      sorter = sorters.get_fzy_sorter(telescope_opts),
-      attach_mappings = function(prompt_bufnr, _)
-        actions.select_default:replace(function()
-          local selection = action_state.get_selected_entry()
-          actions.close(prompt_bufnr)
-          local output = selection.value
-          local pos = vim.api.nvim_win_get_cursor(0)
-          vim.api.nvim_buf_set_text(0, pos[1] - 1, pos[2], pos[1] - 1, pos[2], { output })
-          vim.api.nvim_win_set_cursor(0, { pos[1], pos[2] + 2 })
-        end)
-        return true
-      end,
-    })
-    :find()
+      })
+      :find()
 end
+
+--M.search()
 
 return M
