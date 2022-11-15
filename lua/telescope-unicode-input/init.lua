75ae40f0e226f5a2e38a986438e1a889ca10c026
Manuel Brea <m.brea.carreras@gmail.com>
2022-11-15 16:15:51 +0000

    Made  idempotent

--- lua/telescope-unicode-input/init.lua
+++ lua/telescope-unicode-input/init.lua
@@ -1,10 +1,16 @@
 local M = {}
 
-M.opts = {
-  unicode = {},
-}
+local default = function()
+  return {
+    unicode = {},
+  }
+end
+
+M.opts = default()
 
 M.setup = function(opts)
+  M.opts = default()
+
   if opts.unicode ~= nil then
     for _, entry in ipairs(opts.unicode) do
       if entry.key ~= nil then
